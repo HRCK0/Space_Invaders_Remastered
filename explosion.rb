54b016b6c54d0c0b227dc6e1d5c53fe11b54030c
Mustafa Ali <alimustafa0987@gmail.com>
2018-12-01 13:53:52 -0500

    error fixing

--- explosion.rb
+++ explosion.rb
@@ -3,24 +3,27 @@ class Explosion
     @x = x
     @y = y
     @radius = 30
-    @images = Gosu::Image.load_tiles('SPRITES/explosion.png',62, 62)
+    @images = Gosu::Image.load_tiles('SPRITES/explosion.png',62,62)
     @counter = 1
     @image_index=0
     @finished = false
   end
-
   def draw
     if @image_index < @images.count
       @images[@image_index].draw(@x-@radius,@y-@radius,2)
-      @counter+=1
+      @counter+= 1
+      if @counter==4
+        @image_index+=1
+        @counter = 1
+      end
     else
       @finished = true
     end
 
   end
 
-  def get_finished
-    @finished
+  def getfinished
+    return @finished
   end
 
 end
