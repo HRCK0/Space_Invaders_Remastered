594f931a933e26fbb5e028cb17f438b55317c939
HRCK0 <proka2000@gmail.com>
2018-12-01 11:37:53 -0500

    Added bullets = [] and enemeis = []

--- sector_five.rb
+++ sector_five.rb
@@ -1,16 +1,20 @@
 require 'gosu'
 require_relative 'player'
 require_relative 'enemy'
+require_relative 'bullet'
+require_relative 'explosions'
 
 class SectorFive < Gosu::Window
   WIDTH = 800
   HEIGHT = 600
+  ENEMY_FREQUENCY = 0.05
 
   def initialize
     super(WIDTH, HEIGHT)
     self.caption = 'Space Invaders - Remastered (404 Games)'
     @player = Player.new(self)
-    @enemy = Enemy.new(self)
+    @enemies = []
+    @bullets = []
   end
 
   def update
@@ -18,14 +22,34 @@ class SectorFive < Gosu::Window
     @player.turn_right if button_down?(Gosu::KbRight)
     @player.accelerate if button_down?(Gosu::KbUp)
     @player.move
-    @enemy.move
+
+    @enemies.push Enemy.new(self) if rand < ENEMY_FREQUENCY
+
+    @enemies.each {|enemy| enemy.move} # might crash!!
+    @bullet.each {|bullet| bullet.move}
+
+    @enemies.dup.each do |enemy|
+      @bullets.dup.each do |bullet|
+        distance = Gosu::distance(enemy.x, enemy.y, bullet.x, bullet.y)
+        if distance < enemy.radius + bullet.radius
+          @enemies.delete enemy
+          @bullets.delete bullet
+        end
+      end
+    end
+  end
+
+  def button_down(id)
+    if id == Gosu::KbSpace
+      @bullets.push Bullet.new(self, @player.x, @Player.y, @player_angle)
+    end
   end
 
   def draw
     @player.draw
-    @enemy.draw
+    @enemies.each {|enemy| enemy.draw} # might crash!!
+    @bullets.each {|bullet| bullet.draw} # might crash!!
   end
-
 end
 
 wind = SectorFive.new
